0f38af7c94568f14edda3728292324852b9fda52
Unknown <paulscotch1@gmail.com>
2017-10-13 21:44:57 -0500

    [Code] Finish replacePairsByOdds

--- arquitectura-lab-3.asm
+++ arquitectura-lab-3.asm
@@ -103,14 +103,57 @@
 	syscall
 	
 	replacePairsByOdds:
-		# Move vec0 1 ahead
+	
+		# Save in stack the previous values of t(0..4).
+		addi $sp, $sp, -20
+		sw $t0, 0($sp)
+		sw $t1, 4($sp)
+		sw $t2, 8($sp)
+		sw $t3, 12($sp)
+		sw $t4, 16($sp)
+		
+		# Get initial vec0 & vec1
+		la $t0, vec0($zero)
+		la $t1, vec1($zero)
+		
+		# Move vec0 1 ahead & test vec1 position
+		lw $t2, 0($t0)
+		beq $t2, -9999, exitReplace
+		addi $t0, $t0, 4
+		
+		addi $t4, $zero, 1
 		
 		# Loop through vec0 & replace with vec1
+		loopReplace:
+			lw $t2, 0($t0)
+			lw $t3, 0($t1)
+			beq $t2, -9999, exitReplace
+			beq $t3, -9999, exitReplace
+			bne $t4, 1, continueLoop
+			
+			sw $t3, 0($t0)
+			
+		continueLoop:
+			addi $t0, $t0, 4
+			addi $t1, $t1, 4
+			mul $t4, $t4, -1
+			j loopReplace
+			
+		exitReplace:
 		
-		addi $a0, $zero, 1
-		addi $v0, $zero, 1
-		syscall
-	
+			# Print array			
+			la $a0, vec0($zero)
+			
+			jal printVector
+			
+			# Load previous values of t(0..4) & release stack
+			lw $t0, 0($sp)
+			lw $t1, 4($sp)
+			lw $t2, 8($sp)
+			lw $t3, 12($sp)
+			lw $t4, 16($sp)
+			addi $sp, $sp, 20
+			
 	# To this point the program is done
 	li $v0, 10
 	syscall
